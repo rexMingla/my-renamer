782c20f80f1e89a1f528ffe0d71caf86b31654b9
RexMingla@gmail.com <RexMingla@gmail.com@012800a9-0be0-3348-eea0-bb68149cca81>
2011-04-04 05:55:35 +0000

    restructure and splitting to one class per file were appropriate

---
+++ src/trunk/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# --------------------------------------------------------------------------------------------------------------------
+# Project:             my-renamer
+# Repository:          http://code.google.com/p/my-renamer/
+# License:             Creative Commons GNU GPL v2 (http://creativecommons.org/licenses/GPL/2.0/)
+# Purpose of document: ??
+# --------------------------------------------------------------------------------------------------------------------
