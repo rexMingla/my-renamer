cf59b0ac426d0280b783db62135257809936abe4
RexMingla@gmail.com <RexMingla@gmail.com@012800a9-0be0-3348-eea0-bb68149cca81>
2011-05-02 12:17:00 +0000

    - Minor ui tweaks

--- src/trunk/todo.py
+++ src/trunk/todo.py
@@ -13,8 +13,8 @@
 #    2011-04-29                  Convert config to json (maybe rework save/load objs)
 #    2011-04-29  2011-05-02      Figure out cause of: "QObject::startTimer: QTimer can only be used with threads started with QThread"
 #    2011-05-01  2011-05-01      Enable/disable buttons
-#    2011-05-02                  Save window layout
-#    2011-05-02                  Use dock widgets
+#    2011-05-02  2011-05-02      Save window layout
+#    2011-05-02  2011-05-02      Use dock widgets
 #    2011-05-02                  Improve logging
 
 
