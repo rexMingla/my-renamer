4a952c8095de7cff2a6bd265fd9d27ba0efa87ac
RexMingla@gmail.com <RexMingla@gmail.com@012800a9-0be0-3348-eea0-bb68149cca81>
2013-01-26 05:17:52 +0000

    moved tv, movie and base into media folder

--- src/trunk/tv/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# --------------------------------------------------------------------------------------------------------------------
-# Project:             my-renamer
-# Repository:          http://code.google.com/p/my-renamer/
-# License:             Creative Commons GNU GPL v2 (http://creativecommons.org/licenses/GPL/2.0/)
-# Purpose of document: Module for tv season specific functionality
-# --------------------------------------------------------------------------------------------------------------------
