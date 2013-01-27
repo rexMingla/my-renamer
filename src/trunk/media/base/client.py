02a06d100465d97ceb70b66cbd87d090f5bf8c8c
RexMingla <RexMingla@gmail.com>
2013-01-27 22:51:37 +0000

    - moved from camel case to underscore delimited for variables, member and functions issue 20 - added check for config and cache version - Logs now rolled

--- src/trunk/media/base/client.py
+++ src/trunk/media/base/client.py
@@ -11,97 +11,93 @@ class BaseInfoStoreHolder(object):
   """ container for all of the InfoClients """
   
   def __init__(self):
+    super(BaseInfoStoreHolder, self).__init__()
     self.stores = []
   
-  def addStore(self, store):
-    index = self.getStoreIndex(store)
+  def add_store(self, store):
+    index = self.get_store_index(store)
     if index == -1:
       self.stores.append(store)
     else:
       self.stores[index] = store
     
-  def getStoreIndex(self, name):
-    return next( (i for i, s in enumerate(self.stores) if name == s.prettyName() ), -1)
+  def get_store_index(self, name):
+    return next( (i for i, store in enumerate(self.stores) if name == store.pretty_name() ), -1)
   
-  def getStoreHolder(self, name):
-    return next( (s for s in self.stores if name == s.prettyName() ), None)
+  def get_store_helper(self, name):
+    return next( (store for store in self.stores if name == store.pretty_name() ), None)
   
-  def setAllActive(self, stores):
-    for key in stores:
-      s = self.getStoreHolder(key)
-      if s and s.isAvailable():
-        s.isEnabled = True
-    
-  def getAllActiveNames(self):
-    return [store.prettyName() for store in self.stores if store.isActive()]  
+  def get_all_active_names(self):
+    return [store.pretty_name() for store in self.stores if store.is_active()]  
   
-  def getConfig(self):
+  def get_config(self):
     ret = []
-    for i, s in enumerate(self.stores):
-      ret.append({"name": s.prettyName(), "isEnabled": s.isEnabled, "key": s.key, "index": i})
+    for i, store in enumerate(self.stores):
+      ret.append({"name": store.pretty_name(), "requires_key": store.requires_key, "key": store.key, "index": i})
     return ret
   
-  def setConfig(self, data):
+  def set_config(self, data):
     for i, values in enumerate(data):
-      index = self.getStoreIndex(values["name"])
+      index = self.get_store_index(values["name"])
       if index != -1 and index != i:
         store = self.stores.pop(index)
         self.stores.insert(values["index"], store)
-        store.isEnabled = values["isEnabled"]
+        store.requires_key = values["requires_key"]
         store.key = values["key"]
         
-  """ get info api  """
-  def getInfo(self, searchParams, default=None):
-    return next(self.getInfos(searchParams), ResultHolder(default, "")).info
+  def get_info(self, search_params, default=None):
+    """ get info api  """
+    return next(self.get_all_info(search_params), ResultHolder(default, "")).info
   
-  def getInfos(self, searchParams):
+  def get_all_info(self, search_params):
     """ returns an iterator to ResultHolder objects """
     for store in self.stores:
-      if store.isActive():
-        for info in store.getInfos(searchParams):
-          yield ResultHolder(info, store.sourceName)  
+      if store.is_active():
+        for info in store.get_all_info(search_params):
+          yield ResultHolder(info, store.source_name)  
   
 # --------------------------------------------------------------------------------------------------------------------
-class ResultHolder:
-  def __init__(self, info, sourceName):
+class ResultHolder(object):
+  def __init__(self, info, source_name):
+    super(ResultHolder, self).__init__()
     self.info = info
-    self.sourceName = sourceName
+    self.source_name = source_name
     
 # --------------------------------------------------------------------------------------------------------------------
 class BaseInfoClient(object):
   """ class to retrieve information from an online (or other) resource """
   __metaclass__ = abc.ABCMeta
   
-  def __init__(self, displayName, sourceName, url, hasLib, requiresKey):
+  def __init__(self, display_name, source_name, url, has_lib, requires_key):
     super(BaseInfoClient, self).__init__()
-    self.displayName = displayName #lib used
-    self.sourceName = sourceName #website (or other) its connected to
+    self.display_name = display_name #lib used
+    self.source_name = source_name #website (or other) its connected to
     self.url = url #website
-    self.hasLib = hasLib #is the library available
-    self.requiresKey = requiresKey
+    self.has_lib = has_lib #is the library available
+    self.requires_key = requires_key
     self.key = ""
-    self.isEnabled = True #enabled by user
+    self.is_enabled = True #enabled by user
     
-  def prettyName(self):
-    return "{} ({})".format(self.sourceName, self.displayName)
+  def pretty_name(self):
+    return "{} ({})".format(self.source_name, self.display_name)
   
-  def isAvailable(self):
-    return self.hasLib and (not self.requiresKey or bool(self.key))
+  def is_available(self):
+    return self.has_lib and (not self.requires_key or bool(self.key))
     
-  def isActive(self):
-    return self.isAvailable() and self.isEnabled
+  def is_active(self):
+    return self.is_available() and self.is_enabled
   
   #api stuff
-  def getInfo(self, searchParams):
-    return self._getInfo(searchParams) if self.hasLib else None
+  def get_info(self, search_params):
+    return self._get_info(search_params) if self.has_lib else None
 
-  def getInfos(self, searchParams):
-    return self._getInfos(searchParams) if self.hasLib else None    
+  def get_all_info(self, search_params):
+    return self._get_all_info(search_params) if self.has_lib else None    
 
-  def _getInfo(self, searchParams):
-    infos = self._getInfos(searchParams)
+  def _get_info(self, search_params):
+    infos = self._get_all_info(search_params)
     return infos[0] if infos else None
   
   @abc.abstractmethod
-  def _getInfos(self, searchParams):
+  def _get_all_info(self, search_params):
     pass
